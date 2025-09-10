033181d1801a3b046241b049acfe6d2d376d92e8
Dandara <dandafarss@gmail.com>
2025-09-10 09:47:19 -0300

    pede os valores ao usuário

--- src/PriemeiroPeriodo/ExercicioMtmDiscreta.java
+++ src/PriemeiroPeriodo/ExercicioMtmDiscreta.java
@@ -1,20 +1,26 @@
 package PriemeiroPeriodo;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class ExercicioMtmDiscreta {
     public static void main(String[] args) {
         /*
         Uma loja concede desconto se:
-        O cliente for estudante AND menor de 25 anos,
-        OR se for idoso (≥ 60 anos),
-        Mas NOT se ele já usou desconto na última compra.
-         */
+        O cliente for estudante e menor de 25 anos,
+        ou se for idoso (≥ 60 anos),
+        Mas em nenhum caso se ele já usou desconto na última compra.*/
 
-        boolean clienteEstudante = true;
-        int idadeCliente = 20;
-        boolean usadoDesconto = false;
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Você é estudante? (true/false)");
+        boolean clienteEstudante = entrada.nextBoolean();
+        System.out.println("Qual sua idade?");
+        int idadeCliente = entrada.nextInt();
+        System.out.println("Você já usou o desconto antes? (true/false)");
+        boolean usadoDesconto = entrada.nextBoolean();
 
-        if (((clienteEstudante == true && idadeCliente < 25) || idadeCliente>=60) && !usadoDesconto){
+        if (((clienteEstudante && idadeCliente < 25) || idadeCliente>=60) && !usadoDesconto){
             System.out.println("Então a loja concede desconto");
         }else{
             System.out.println("A loja não concede desconto");
